f58afb07f742bf8b3f4bae3e03a67985c53c2820
djfdmator <djfdmator@naver.com>
2022-01-25 17:40:39 +0900

    LV2. 더 맵게

---
+++ Programmers/Level_2/더 맵게/source.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
+    while(pq.top() <= K)
+    {
+        if(pq.size() == 1) return -1;
+        int first = pq.top(); pq.pop();
+        int second = pq.top(); pq.pop();
+        
+        pq.push(first + 2 * second);
+        answer++;
+    }
+    
+    return answer;
+}
