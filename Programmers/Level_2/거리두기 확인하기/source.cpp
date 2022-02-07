7b25ef6c9eaaeeafc45bdee8f8cb0e8ce4a5a0c7
djfdmator <djfdmator@naver.com>
2022-02-07 17:55:13 +0900

    LV2. 거리두기 확인하기

---
+++ Programmers/Level_2/거리두기 확인하기/source.cpp
@@ -0,0 +1,79 @@
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
+
+int judge(vector<vector<string>> &places, int i, int j, int k)
+{
+    int res = 1;
+    if(places[i][j][k] == 'P')
+    {
+        res = 0;
+    }
+    else if(places[i][j][k] == 'O')
+    {
+        places[i][j][k] = '1';
+    }
+    else if(places[i][j][k] == '1')
+    {
+        places[i][j][k] = '2';
+    }
+    return res;
+}
+
+vector<int> solution(vector<vector<string>> places) {
+    vector<int> answer;
+    
+    for(int i = 0; i < 5; i++)
+    {
+        int res = 1;
+        for(int j = 0; j < 5; j++)
+        {
+            cout << endl;
+            for(int k = 0; k < 5; k++)
+            {
+                if(places[i][j][k] == 'P')
+                {
+                    if(k + 1 < 5) {
+                        res = judge(places, i, j, k + 1);
+                        if(res == 0) break;
+                    }
+                    if(k - 1 > 0) {
+                        res = judge(places, i, j, k - 1);
+                        if(res == 0) break;
+                    }
+                    if(j + 1 < 5) {
+                        res = judge(places, i, j + 1, k);
+                        if(res == 0) break;
+                    }
+                    if(j - 1 > 0) {
+                        res = judge(places, i, j - 1, k);
+                        if(res == 0) break;
+                    }
+                }
+            }
+            if(res == 0) break;
+        }
+
+        if(res == 1)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                for(int k = 0; k < 5; k++)
+                {
+                    if(places[i][j][k] == '2')
+                    {
+                        res = 0;
+                        break;
+                    }
+                }
+                if(res == 0) break;
+            }
+        }
+        
+        answer.push_back(res);
+    }
+    return answer;
+}
