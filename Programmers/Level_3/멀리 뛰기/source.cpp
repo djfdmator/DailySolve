86f7bb73a6efb866e2d7561796b6225c34090515
djfdmator <djfdmator@naver.com>
2022-02-26 15:47:44 +0900

    LV3. 멀리 뛰기

---
+++ Programmers/Level_3/멀리 뛰기/source.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    long long answer = 0;
+    
+    vector<long long> dp(n);
+    dp[0] = 1;
+    dp[1] = 2;
+    for(int i = 2; i < n; i++)
+    {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+    
+    return dp[n - 1];
+}
